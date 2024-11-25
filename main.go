c6a56750a8521049c45c7f72136bfe0a2449f3e7
Jatin Mehrotra <jatin.mehrotra@CK0662-001.local>
2024-11-25 17:11:18 +0900

    💚 test: testing connection

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World from eks "}
+		res := &response{Message: "Hello World from jatin"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
